e95f57e048c09a44f750774aba34e8443d85ffd4
krlim417 <krlim417@gmail.com>
2017-11-20 12:41:54 -0800

    Create home page and add it to index.html

---
+++ Angular-Client/src/components/home.js
@@ -0,0 +1,9 @@
+angular.module('happeningsApp')
+
+  .controller('homeCtrl', () => {
+
+  })
+  .component('app', {
+    controller: 'homeCtrl',
+    templateUrl: '../../templates/home.html',
+  });
---
+++ Angular-Client/src/services/items.js
@@ -0,0 +1,3 @@
+// angular.module('happeningsApp', [])
+// .service('landingService', () => {
+// })
