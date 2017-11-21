66da5d123397bc6d9d9034176a48840a58fb3577
Jeff Tsai <jefftsai0216@gmail.com>
2017-11-21 15:18:24 -0800

    added tests for db

--- test/test.js
+++ test/test.js
@@ -1,5 +1,66 @@
 const expect = require('chai').expect;
 const server = require('../Server/index.js');
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// Create a new schema that accepts a 'name' object.
+// 'name' is a required field
+const testSchema = new Schema({
+  name: { type: String, required: true },
+});
+
+// Create a new collection called 'Name'
+const Name = mongoose.model('Name', testSchema);
+
+describe('Database Tests', function () {
+  // Before starting the test, create a sandboxed database connection
+  // Once a connection is established invoke done()
+
+  before(function (done) {
+    mongoose.connect('mongodb://localhost/testDatabase');
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error'));
+    db.once('open', function () {
+      console.log('We are connected to test database!');
+      done();
+    });
+  });
+
+  describe('Test Database', function () {
+    // Save object with 'name' value of 'Mike"
+    it('New name saved to test database', function (done) {
+      const testName = Name({
+        name: 'Mike',
+      });
+      testName.save(done);
+    });
+    it('Dont save incorrect format to database', function (done) {
+      // Attempt to save with wrong info. An error should trigger
+      const wrongSave = Name({
+        notName: 'Not Mike',
+      });
+      wrongSave.save(err => {
+        if (err) { return done(); }
+        throw new Error('Should generate error!');
+      });
+    });
+    it('Should retrieve data from test database', function (done) {
+      // Look up the 'Mike' object previously saved.
+      Name.find({ name: 'Mike' }, (err, name) => {
+        if (err) { throw err; }
+        if (name.length === 0) { throw new Error('No data!'); }
+        done();
+      });
+    });
+  });
+  // After all tests are finished drop database and close connection
+  after(function (done) {
+    mongoose.connection.db.dropDatabase(function () {
+      mongoose.connection.close(done);
+    });
+  });
+});
 
 // describe("Description of the test", function() {
 //   describe("more specific detail of each test", function() {
@@ -7,3 +68,4 @@ const server = require('../Server/index.js');
 //     });
 //   });
 // });
+
