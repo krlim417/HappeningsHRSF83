9e01c996afa5194a0591d630d623d43ca453b037
Andrew Chen <andrewchen.c44@gmail.com>
2017-11-22 16:33:03 -0800

    Implemented button

--- Angular-Client/src/components/home.js
+++ Angular-Client/src/components/home.js
@@ -2,8 +2,9 @@ angular.module('app')
 
   .component('home', {
     bindings: {},
-    controller: function homeCtrl(landingRedirector) {
+    controller: function homeCtrl(landingRedirector, search) {
       this.recommendations = landingRedirector.result;
+      this.search = search.filter;
     },
     templateUrl: '/src/templates/home.html',
   });
---
+++ Angular-Client/src/services/search.js
@@ -0,0 +1,15 @@
+angular.module('app')
+  .service('search', function landingRedirect($http) {
+
+    this.filter = (input) => {
+      console.log("calling service correctly");
+      $http.get('/search', { data: { input } })
+        .then((response) => {
+          console.log('Successfully posted to /home for redirect from the landing page.');
+          this.result = response.data;
+        })
+        .catch(() => {
+          console.log('Failed to post to /home for redirect from the landing page.');
+        });
+    };
+  });
