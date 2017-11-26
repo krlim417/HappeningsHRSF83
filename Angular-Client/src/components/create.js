3a472da4059ce1cea4fd8b58a2765886cc29bc88
krlim417 <krlim417@gmail.com>
2017-11-26 15:22:07 -0800

    Render some items on edit page (functionality incomplete)

--- Angular-Client/src/components/create.js
+++ Angular-Client/src/components/create.js
@@ -3,19 +3,19 @@ angular.module('app')
     bindings: {
       changeView: '<',
     },
-    controller: function createCtrl(search) {
+    controller: function createCtrl(search, findEventByReference) {
       this.post = {
-        name: '',
-        city: '',
-        date: '',
-        time: '',
-        description: '',
-        imgUrl: '',
-        cost: 0,
-        address: '',
-        intensity: 1,
-        duration: '',
-        reference: '',
+        name: '' || findEventByReference.editEventData.name,
+        city: '' || findEventByReference.editEventData.city,
+        date: '' || findEventByReference.editEventData.date,
+        time: '' || findEventByReference.editEventData.time,
+        description: '' || findEventByReference.editEventData.description,
+        imgUrl: '' || findEventByReference.editEventData.imgUrl,
+        cost: 0 || findEventByReference.editEventData.cost,
+        address: '' || findEventByReference.editEventData.address,
+        intensity: 1 || findEventByReference.editEventData.intensity,
+        duration: '' || findEventByReference.editEventData.duration,
+        reference: '' || findEventByReference.editEventData.reference,
       };
       this.click = () => {
         search.save(this.post, this.changeView);
