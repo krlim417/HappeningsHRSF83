a240b75678601e672de06346792280bbf628dac6
krlim417 <krlim417@gmail.com>
2017-11-26 19:42:56 -0800

    Render edit data to edit page

--- Angular-Client/src/components/create.js
+++ Angular-Client/src/components/create.js
@@ -1,10 +1,25 @@
 angular.module('app')
   .component('create', {
     bindings: {
+      view: '<',
       changeView: '<',
     },
     controller: function createCtrl(search, findEventByReference) {
       this.post = {
+        name: '',
+        city: '',
+        date: '',
+        time: '',
+        description: '',
+        imgUrl: '',
+        cost: 0,
+        address: '',
+        intensity: 1,
+        duration: '',
+        reference: '',
+      };
+
+      this.postEdit = {
         name: '' || findEventByReference.editEventData.name,
         city: '' || findEventByReference.editEventData.city,
         date: '' || findEventByReference.editEventData.date,
@@ -13,12 +28,17 @@ angular.module('app')
         imgUrl: '' || findEventByReference.editEventData.imgUrl,
         cost: 0 || findEventByReference.editEventData.cost,
         address: '' || findEventByReference.editEventData.address,
-        intensity: 1 || findEventByReference.editEventData.intensity,
+        intensity: '' || findEventByReference.editEventData.intensity,
         duration: '' || findEventByReference.editEventData.duration,
         reference: '' || findEventByReference.editEventData.reference,
       };
+
       this.click = () => {
-        search.save(this.post, this.changeView);
+        if (this.view === 'create') {
+          search.save(this.post, this.changeView);
+        } else if (this.view === 'edit') {
+          search.save(this.postEdit, this.changeView);
+        }
       };
     },
     templateUrl: '/src/templates/create.html',
